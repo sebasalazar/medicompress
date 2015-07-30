fec9df00ea4379951dbcaa6c4331d31848eb4dc2
Sebastián Salazar Molina <sebasalazar@gmail.com>
2015-07-30 12:39:28 -0300

    Fix

--- src/main/java/cl/medipacs/compress/run/App.java
+++ src/main/java/cl/medipacs/compress/run/App.java
@@ -1,14 +1,19 @@
 package cl.medipacs.compress.run;
 
 import cl.medipacs.compress.servicio.ServicioDB;
+import java.io.Serializable;
 import java.util.List;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Sebastián Salazar Molina <ssalazar@experti.cl>
  */
-public class App {
+public class App implements Serializable {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     /**
      * @param args the command line arguments
@@ -26,11 +31,15 @@ public class App {
                 sb.append(String.format("%s ", exm));
             }
             String linea = String.format("/usr/bin/zip -5 /srv/web/medipacs.cl/www/htdocs/zip/%s.zip %s", nombre, StringUtils.trimToEmpty(sb.toString()));
+            logger.debug(linea);
 
             Process p = Runtime.getRuntime().exec(linea);
             p.waitFor();
         } catch (Exception e) {
-            System.out.println(String.format("Error al procesar: %s", e.toString()));
+            String mensaje = String.format("Error al procesar: %s", e.toString());
+            logger.error(mensaje);
+            logger.debug(mensaje, e);
+            System.out.println(mensaje);
         }
     }
 
