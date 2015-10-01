c6b140a25cd2c78259240f02fcb7e636d53b4e6e
Sebastián Salazar Molina <sebasalazar@gmail.com>
2015-10-01 18:02:27 -0300

    Fix

--- src/main/java/cl/medipacs/compress/run/App.java
+++ src/main/java/cl/medipacs/compress/run/App.java
@@ -24,45 +24,49 @@ public class App implements Serializable {
      */
     public static void main(String[] args) {
         try {
-            Date ahora = new Date();
-            long tiempoInicio = ahora.getTime();
-
-            ServicioDB servicio = new ServicioDB();
-
             Long id = Long.parseLong(args[0]);
             String nombre = StringUtils.trimToEmpty(args[1]);
 
-            List<String> consultarExamenes = servicio.consultarExamenes(id, nombre);
-            StringBuilder sb = new StringBuilder();
-            for (String exm : consultarExamenes) {
-                sb.append(String.format("%s ", exm));
+            if (id != null && StringUtils.isNotBlank(nombre)) {
+                Date ahora = new Date();
+                long tiempoInicio = ahora.getTime();
+
+                ServicioDB servicio = new ServicioDB();
+
+                List<String> consultarExamenes = servicio.consultarExamenes(id, nombre);
+                StringBuilder sb = new StringBuilder();
+                for (String exm : consultarExamenes) {
+                    sb.append(String.format("%s ", exm));
+                }
+
+                String nombreArchivo = String.format("/srv/web/medipacs.cl/www/htdocs/zip/%s%d.zip", nombre, id);
+                File zip = new File(nombreArchivo);
+
+                int codigo = -1;
+                if (!zip.isFile()) {
+                    String linea = String.format("/usr/bin/zip -5 %s %s", nombreArchivo, StringUtils.trimToEmpty(sb.toString()));
+                    logger.debug(linea);
+
+                    Process p = Runtime.getRuntime().exec(linea);
+                    p.waitFor();
+                    codigo = p.exitValue();
+                }
+
+                // Objeto Compresión, unica finalidad registro de examenes
+                long tiempoFin = new Date().getTime();
+                Compresion compresion = new Compresion();
+                compresion.setArchivo(nombreArchivo);
+                compresion.setCantidadExamenes(consultarExamenes.size());
+                compresion.setFecha(ahora);
+                compresion.setPacienteId(id);
+                compresion.setCodigoSalida(codigo);
+                compresion.setTiempoProcesamiento(new BigDecimal((tiempoFin - tiempoInicio) / 1000.0));
+                boolean okGuardado = servicio.guardar(compresion);
+
+                logger.info("Archivo: '{}' # ok: '{}' # db: '{}'", nombreArchivo, zip.isFile(), okGuardado);
+            } else {
+                System.out.println("Argumentos inválidos");
             }
-
-            String nombreArchivo = String.format("/srv/web/medipacs.cl/www/htdocs/zip/%s%d.zip", nombre, id);
-            File zip = new File(nombreArchivo);
-
-            int codigo = -1;
-            if (!zip.isFile()) {
-                String linea = String.format("/usr/bin/zip -5 %s %s", nombreArchivo, StringUtils.trimToEmpty(sb.toString()));
-                logger.debug(linea);
-
-                Process p = Runtime.getRuntime().exec(linea);
-                p.waitFor();
-                codigo = p.exitValue();
-            }
-
-            // Objeto Compresión, unica finalidad registro de examenes
-            long tiempoFin = new Date().getTime();
-            Compresion compresion = new Compresion();
-            compresion.setArchivo(nombreArchivo);
-            compresion.setCantidadExamenes(consultarExamenes.size());
-            compresion.setFecha(ahora);
-            compresion.setPacienteId(id);
-            compresion.setCodigoSalida(codigo);
-            compresion.setTiempoProcesamiento(new BigDecimal((tiempoFin - tiempoInicio) / 1000.0));
-            boolean okGuardado = servicio.guardar(compresion);
-
-            logger.info("Archivo: '{}' # ok: '{}' # db: '{}'", nombreArchivo, zip.isFile(), okGuardado);
         } catch (Exception e) {
             String mensaje = String.format("Error al procesar: %s", e.toString());
             logger.error(mensaje);
